3c7a06c51d90164d31af77d3648bec8bbc6d4125
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-24 18:27:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -37,7 +37,7 @@ class String
 =end   
 
   def count_sentences
-   new_array = self.split(/[.?!]/).reject {|string| string.empty?}
-    return new_array.length
+   arr = self.split(/[.?!]/).reject {|string| string.empty?}
+    return arr.length
   end
 end
